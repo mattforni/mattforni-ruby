88ae10bbdfa7be21e6bf9551efff9fdff475ef43
forni <forni@forni-VGN-C190G.(none)>
2011-07-08 20:49:27 -0700

    initial commit - default rails scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mattforni::Application.config.secret_token = '56b452ee779d524eb2b72ed56323c2da590bc7e73c94670f7d59ca47bc6c7021027b5725e2f902ae0ac80ed3c6fb9336a13c1ef81abe540ea84e8440aa834c2c'
