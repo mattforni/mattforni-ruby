702d55f95c33f89c8d78789ef4d49ba66746987f
Matt Fornaciari <mattforni@gmail.com>
2014-03-14 22:05:41 -0700

    + added comments to finance.js + moved quote to ajax_controller.rb

--- app/assets/javascripts/finance.js
+++ app/assets/javascripts/finance.js
@@ -1,46 +1,82 @@
+/**
+ * This file contains all JavaScript logic pertinent to the finance pages.
+ * 
+ * @author Matt Fornaciari (mattforni@gmail.com)
+ */
+
 //= require chart
 
 var QUOTE_URL = '/finance/quote';
 
+/**
+ * Creates, scopes and binds the FinanceController to the angular module
+ * defined in app/assets/javascripts/application.js. It should be noted the
+ * '$scopes' declaration is necessary for clarification to the angular injector
+ * due to the minfication process.
+ */
 mattforni.controller('FinanceController', ['$scope',
     function($scope) {
         $scope.accountSize = 0;
         $scope.lastTrade = 0;
         $scope.numShares = 0;
         $scope.positionSize = 0;
+        $scope.possibleLoss = 0;
         $scope.risk = 5;
         $scope.stop = 25; 
-        $scope.symbol = '';
+        $scope.stopPrice = 0;
+        $scope.symbol = "";
 
         var a = 0;
         var r = 0;
 
-        $scope.update = function(digest) {
-            a = $scope.stop == 0 ? 0 : 100.0 / $scope.stop;
-            r = $scope.accountSize * ($scope.risk / 100.0);
-            $scope.positionSize = a * r;
-            $scope.numShares = $scope.lastTrade == 0 ? 0 : $scope.positionSize / $scope.lastTrade;
-            if (digest) { $scope.$digest(); }
-        }
 
+        /**
+         * Formats the stock symbol and then attempts to retrieves a quote for
+         * said symbol, updating the lastTrade value on success.
+         */
         $scope.quote = function() {
-            // Makes symbol upper case for compliance 
+            if (!$scope.symbol) { return; }
+            // Make symbol upper case for compliance 
             $scope.symbol = $scope.symbol.toUpperCase();
-            $.ajax({
+            var request = $.ajax({
                 accepts: 'json',
                 data: {
                     symbol: $scope.symbol 
                 },
                 dataType: 'json',
                 type: 'GET',
-                success: function(data, code, jqXHR) {
-                    $scope.lastTrade = data['lastTrade'];
-                    $scope.update(true);
-                },
                 url: QUOTE_URL
-                // TODO switch this to use .done()
             });
-        }
-    }
+
+            // Bind the success handler to the ajax request object
+            request.done(function(data, code, jqXHR) {
+                $scope.lastTrade = data['lastTrade'];
+                $scope.update(true);
+            });
+        } // End $scope.quote
+
+        /**
+         * Updates all variables associated with position sizing. 
+         *
+         * @params {boolean} digest Whether or not to digest the scope.
+         */
+        $scope.update = function(digest) {
+            a = $scope.stop == 0 ? 0 : 100.0 / $scope.stop;
+            r = $scope.accountSize * ($scope.risk / 100.0);
+            $scope.positionSize = a * r;
+
+            // If lastTrade is 0, ignore numShares and stopPrice
+            if ($scope.lastTrade == 0) {
+                $scope.numShares = 0;
+                $scope.stopPrice = 0;
+            } else  { // Else calculate the new values
+                $scope.numShares = $scope.positionSize / $scope.lastTrade;
+                $scope.stopPrice = ((100.0 - $scope.stop)/100.0) * $scope.lastTrade;
+            }
+
+            $scope.possibleLoss = ($scope.lastTrade - $scope.stopPrice) * $scope.numShares;
+            if (digest) { $scope.$digest(); }
+        } // End $scope.update
+    } // End anonymous FunctionController function
 ]);
 
--- app/controllers/finance/ajax_controller.rb
+++ app/controllers/finance/ajax_controller.rb
@@ -3,11 +3,17 @@ require 'stocks'
 module Finance
 	class AjaxController < ApplicationController
 		def quote
-			@quote = Stocks.get_quote(params[:symbol], [:lastTrade])
-			
-			# TODO update to reject HTML requests
+			@quote = Stocks.get_quote(params[:symbol], [:lastTrade, :date])
+      			
+			# TODO update to reject HTML requests, maybe clean up json response
 			respond_to do |format|
-				format.json { render json: @quote }
+				format.json do 
+          if @quote[:date] == Stocks::NA
+            render json: {}, status: 400 if @quote[:date] == Stocks::NA
+          else
+            render json: @quote
+          end
+        end 
 			end
 		end
 	end
