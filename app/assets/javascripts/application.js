6b15709d73f87f13dfecd7b4d024c2bd8f88c927
Matt Fornaciari <mattforni@gmail.com>
2014-04-15 19:50:30 -0700

    + updating splash for fun + updating stop view for readability

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,3 +6,13 @@
 
 var mattforni = angular.module('mattforni', []);
 
+$( document ).ready(function() {
+  $("#middle").hover(function() {
+    $(this).css("opacity", 1);
+  },
+  function() {
+    $(this).css("opacity", 0.05);
+  });
+});
+
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,14 @@ module ApplicationHelper
     return value.nil? ? alternative : value
   end
 
+  def dec_display(decimal)
+    number_with_precision(decimal, delimiter: ',', precision: 3)
+  end
+
+  def time_display(time)
+    time.strftime("%^b %d %Y %H:%M")
+  end
+
   def price_class(value)
     return value > 0 ? 'positive' : (value == 0 ? '' : 'negative')
   end
