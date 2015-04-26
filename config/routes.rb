0570ade6c22485f033494d93f70bd02c15db85bd
Matt Fornaciari <mattforni@gmail.com>
2015-04-25 18:05:40 -0700

    Updated stopped_out mailer

--- config/routes.rb
+++ config/routes.rb
@@ -21,8 +21,8 @@ Mattforni::Application.routes.draw do
       },
       to: 'finance#historical'
     get '/last_trade', to: 'finance#last_trade'
-    get '/positions', to: 'finance#positions'
-    get '/sizing', to: 'finance#sizing'
+    get '/positions', as: 'finance_positions', to: 'finance#positions'
+    get '/sizing', as: 'finance_sizing', to: 'finance#sizing'
   end
   namespace 'finance' do
     resources :holdings, except: :index
