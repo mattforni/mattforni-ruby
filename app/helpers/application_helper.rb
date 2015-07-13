53a133f9f0655da7b643e96767f637c2e19624fb
Matthew <mfornaciari@salesforce.com>
2015-07-13 11:24:26 -0700

    [Layout] Abstracted common logic for header and added asset existence check before rendering

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,10 @@ module ApplicationHelper
     return value.nil? ? alternative : value
   end
 
+  def asset_exists?(filename, extension)
+    !Mattforni::Application.assets.find_asset("#{filename}.#{extension}").nil?
+  end
+
   def currency_display(currency)
     number_to_currency(currency, delimiter: ',', precision: 2, negative_format: '-%u%n')
   end
@@ -21,16 +25,16 @@ module ApplicationHelper
     number_with_precision(decimal, delimiter: ',', precision: precision)
   end
 
+  def external_image(name, link)
+    render partial: 'external/logo', locals: {name: name, link: link}
+  end
+
   def price_class(value)
     return value > 0 ? 'positive' : (value == 0 ? '' : 'negative')
   end
 
   def time_display(time)
     time.strftime("%^b %d %Y %H:%M")
-  end
-
-  def external_image(name, link)
-    render partial: 'external/logo', locals: {name: name, link: link}
   end
 end
 
