ab6680d7b6fa1142a0058b428f25f9252d87ff4a
Matthew Fornaciari <mattforni@gmail.com>
2015-08-25 01:48:55 -0700

    [RSpec] Moved mailer test stubs to rspec

---
+++ spec/mailers/stop_mailer_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe StopMailer do
+
+end
+
--- test/mailers/stop_mailer_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-# TODO add tests for the stop mailer
-class StopMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
